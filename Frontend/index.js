e95d0a47eb1782e2a7a38fda4223f64c419a8eeb
jacinta246 <jacinta246@yahoo.com>
2020-08-10 12:45:01 -0400

    I tried

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,6 @@
 // Code your solution here
+const container = document.querySelector('container');
+
+container.onclick = function (container) {
+    container.addEventListener("click", display);
+  }
