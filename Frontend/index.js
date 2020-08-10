37b2aa13775da1d08c4dd4c1bdee3a791a6e3810
Zeynab-j <zeina2025@gmail.com>
2020-08-10 12:34:22 -0400

    done

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+const showUL = document.getElementById('shoe-list')
+
+fetch('http://localhost:3000/shoes')
+.then(res => res.json())
+.then(shoeArray => {
+    shoeArray.forEach(eachShoe) => {
+        turnShoeToHtml(eachShoe)
+        
+    });
+    
+}
+
+let turnShoeToHtml = (theShoe)
+
+
+
+
+let reviewForm = document.querySelector("#reviews-list")
+
+reviewForm.addEventListener("submit", (event) )
+ event.defaultPrevented()
+
+
+
+
+
+
+POST `http://localhost:3000/shoes/${shoe_id}/reviews`
+
+  Required Headers:
+  {
+    'Content-Type': 'application/json'
+  }
+
+  Required Body:
+  {
+    content: 
+  }
+
+]
+  }
