e0e1404f5fc0099eb2a4da4f1fe0420f8d5cbb63
Fraciscv <Francisco.Andaur@icloud.com>
2020-08-10 10:45:20 -0400

    set variable for shoeList, formContainer, reviewList

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,8 @@
 // Code your solution here
+const shoeList = document.querySelector('#shoe-list')
+const formContainer = document.querySelector('#form-container')
+const reviewList = document.querySelector('#reviews-list')
+
+console.log(shoeList)
+console.log(formContainer)
+console.log(reviewList)
